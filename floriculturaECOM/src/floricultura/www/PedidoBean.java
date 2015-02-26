3af55c13dc1eab9c1c0af32fea32dc34289ce4cc
daniel.pitagoras@gmail.com <daniel.pitagoras@gmail.com@b6e99e39-d21a-9b7a-9446-459ed453c3e0>
2015-02-26 18:46:03 +0000

    - Praticamento tudo funcionando, acertando BUG de endereço -  *** Múltiplas respostas quando pesquisa por descrição ***

--- floriculturaECOM/src/floricultura/www/PedidoBean.java
+++ floriculturaECOM/src/floricultura/www/PedidoBean.java
@@ -4,6 +4,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 
 import org.hibernate.Session;
@@ -15,6 +16,7 @@ import java.lang.StringBuffer;
 
 import floricultura.pedido.*;
 import floricultura.usuario.Usuario;
+import floricultura.usuario.UsuarioRN;
 import floricultura.util.ContextoUtil;
 import floricultura.endereco.Endereco;
 import floricultura.itemPedido.ItemPedido;
@@ -31,7 +33,6 @@ public class PedidoBean {
 	private String enderecoAgrupado;
 	private ItemPedido itemPedido;
 	private List<ItemPedido> listaItemPedido;
-	//private List<ItemPedido> listaItemPedido = new ArrayList<ItemPedido>();
 	
 	public String getEnderecoAgrupado() {
 		
@@ -40,7 +41,13 @@ public class PedidoBean {
 	
 	public String salvar() {
 		
+		// --- Parte de gravação do Pedido no banco.
+		float pesoTotPedido = 0;
+		float precoTotPedido = 0;
+		//int codPedido = 0;
+		
 		PedidoRN pedidoRN = new PedidoRN();
+		this.pedidoEmEdicao.setUsuario(getUsuarioLogado());
 		this.pedidoEmEdicao.setDataPedido(new Date());
 		
 		StringBuffer strBuffer = new StringBuffer();
@@ -65,23 +72,55 @@ public class PedidoBean {
 		this.listaItemPedido = (ContextoUtil.getContextoBean().getListaItemPedido());
 		
 		if (listaItemPedido == null) {
-			return null;
+				System.out.println("* * * Erro obtendo lista de itens pedidos. * * *");
+				return "PedidoNSalvo";
 		} else { 
 			for (int i = 0; i < listaItemPedido.size(); i++){
-				System.out.println(listaItemPedido.get(i).getNome());
+				pesoTotPedido = pesoTotPedido + listaItemPedido.get(i).getPesoTotal();
+				precoTotPedido = precoTotPedido + listaItemPedido.get(i).getPrecoTotal();
 			}
 		}
-		//this.pedidoEmEdicao.setPesoTotal();
-		//this.pedidoEmEdicao.setPrecoTotal();
-		//pedidoRN.salvar(pedidoEmEdicao);
-		return null;
+		
+		this.pedidoEmEdicao.setPesoTotal(pesoTotPedido);
+		this.pedidoEmEdicao.setPrecoTotal(precoTotPedido);
+		pedidoRN.salvar(pedidoEmEdicao);
+		// Jogar para variável pq o valor é perdido após prox iteração do Hibernate.
+		//codPedido = pedidoEmEdicao.getCodigo();
+		
+		// --- Parte de gravação dos itens do Pedido vinculados ao Banco.
+		
+		ItemPedidoRN itemPedidoRN = new ItemPedidoRN();
+		
+		if (listaItemPedido == null) {
+			System.out.println(" * * * Erro gravando os itens do pedido. * * *");
+			return "PedidoNSalvo";
+		} else {
+			for (int i = 0; i < listaItemPedido.size(); i++) {
+				itemPedido = listaItemPedido.get(i);
+				itemPedido.setPedido(pedidoEmEdicao);
+				itemPedidoRN.salvar(itemPedido);
+			}
+		}
+		
+		return "PedidoSalvo";
 	}
+	
+	
 
 	public void setEnderecoAgrupado(String enderecoAgrupado) {
 		this.enderecoAgrupado = enderecoAgrupado;
 	}
 
 	public Usuario getUsuarioLogado() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext external = context.getExternalContext();
+		String login = external.getRemoteUser();
+		if (this.usuarioLogado == null || !login.equals(this.usuarioLogado.getLogin())) {
+			if (login != null) {
+				UsuarioRN usuarioRN = new UsuarioRN();
+				this.usuarioLogado = usuarioRN.buscarPorLogin(login);
+			}
+		}
 		return usuarioLogado;
 	}
 
