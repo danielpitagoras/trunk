0aefde895260e28457a89f02a8cd197822141378
daniel.pitagoras@gmail.com <daniel.pitagoras@gmail.com@b6e99e39-d21a-9b7a-9446-459ed453c3e0>
2015-02-26 12:49:16 +0000

    - Ponto antes * Gravação de itens de pedido e pedido no Banco de Dados

--- floriculturaECOM/src/floricultura/www/PedidoBean.java
+++ floriculturaECOM/src/floricultura/www/PedidoBean.java
@@ -6,25 +6,68 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
-import floricultura.pedido.Pedido;
+import org.hibernate.Session;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.lang.StringBuffer;
+
+import floricultura.pedido.*;
 import floricultura.usuario.Usuario;
 import floricultura.util.ContextoUtil;
 import floricultura.endereco.Endereco;
+import floricultura.itemPedido.ItemPedido;
+import floricultura.itemPedido.ItemPedidoRN;
 
 @ManagedBean(name = "pedidoBean")
 @SessionScoped
 
 public class PedidoBean {
 
-	private Pedido pedidoEmEdicao;
+	private Pedido pedidoEmEdicao = new Pedido();
 	private Usuario usuarioLogado;
 	private Endereco enderecoSelecionado;
 	private String enderecoAgrupado;
+	private ItemPedido itemPedido;
+	private List<ItemPedido> listaItemPedido = new ArrayList<ItemPedido>();
 	
 	public String getEnderecoAgrupado() {
 		
-		
 		return enderecoAgrupado;
+	}
+	
+	public String salvar() {
+		
+		PedidoRN pedidoRN = new PedidoRN();
+		this.pedidoEmEdicao.setDataPedido(new Date());
+		
+		StringBuffer strBuffer = new StringBuffer();
+			strBuffer.append("Cep: ");
+			strBuffer.append(enderecoSelecionado.getCep());
+			strBuffer.append(" - Rua: ");
+			strBuffer.append(enderecoSelecionado.getRua());
+			strBuffer.append(" - Nœmero: ");
+			strBuffer.append(enderecoSelecionado.getNumero());
+			strBuffer.append(" - Complemento: ");
+			strBuffer.append(enderecoSelecionado.getComplemento());
+			strBuffer.append(" - Bairro");
+			strBuffer.append(enderecoSelecionado.getBairro());
+			strBuffer.append(" - Cidade");
+			strBuffer.append(enderecoSelecionado.getCidade());
+			strBuffer.append(" - Estado");
+			strBuffer.append(enderecoSelecionado.getEstado());
+			strBuffer.append(" - Pa’s");
+			strBuffer.append(enderecoSelecionado.getPais());
+			
+		pedidoEmEdicao.setEnderecoEntrega(strBuffer.toString());
+		
+		
+		//this.pedidoEmEdicao.setPesoTotal();
+		//this.pedidoEmEdicao.setPrecoTotal();
+		//pedidoRN.salvar(pedidoEmEdicao);
+		return null;
+		
 	}
 
 	public void setEnderecoAgrupado(String enderecoAgrupado) {
